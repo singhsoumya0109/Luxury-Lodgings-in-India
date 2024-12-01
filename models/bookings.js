cd41f1c6bdfc112950c4b1b1987c3fc02eb74e83
singhsoumya0109 <singhsoumya1103@gmail.com>
2024-12-01 12:28:15 +0530

    Update bookings.js

--- models/bookings.js
+++ models/bookings.js
@@ -26,30 +26,3 @@ const BookingSchema = new Schema({
 
 module.exports = mongoose.model('Booking', BookingSchema);
 
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const BookingSchema = new Schema({
-//     author: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'User'
-//     },
-//     hotel: {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Hotel'
-//     },
-//     checkin: Date,
-//     checkout: Date,
-//     rooms:Number,
-//     adults:Number,
-//     children:Number,
-//     price: Number
-// }, {
-//     timestamps: true 
-// });
-
-// module.exports = mongoose.model('Booking', BookingSchema);
-
-
-
-
